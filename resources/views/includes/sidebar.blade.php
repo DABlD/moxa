9e3ba7270768ba4ccba56fbf7b967187bdb91d8b
David Mendoza <davidmendozaofficial@gmail.com>
2022-10-22 02:15:38 +0800

    fix sidebar size
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{ url("/") }}" class="brand-link">
         <center>
-            <img src="{{ asset("images/moxa_loga2.jpg") }}" alt="AMR" class="brand-image elevation-3" style="opacity: .8">
+            <img src="{{ asset("images/moxa_loga2.jpg") }}" alt="AMR" class="brand-image elevation-3" style="opacity: .8; width: 140px;">
             <span class="brand-text font-weight-light">ㅤ</span>
         </center>
     </a>
