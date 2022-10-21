6ff82396245ed64ee39581510a69b38786346368
David Mendoza <davidmendozaofficial@gmail.com>
2022-10-21 20:27:43 +0800

    fix number to integer
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/migrations/2022_07_09_235715_create_transaction_types_table.php
+++ database/migrations/2022_07_09_235715_create_transaction_types_table.php
@@ -19,7 +19,7 @@ class CreateTransactionTypesTable extends Migration
             $table->integer('admin_id')->unsigned();
             $table->string('type');
             $table->string("operator")->nullable();
-            $table->number("demand")->default(0)->nullable();
+            $table->integer("demand")->default(0)->nullable();
             $table->boolean('inDashboard')->default(false);
             $table->boolean('canDelete')->default(true);
 
