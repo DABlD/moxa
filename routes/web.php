dad158890ef35d4cdfa06c8c75e7aa7de01f8fc9
David Mendoza <davidmendozaofficial@gmail.com>
2022-10-21 23:22:10 +0800

    fix api link
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function(){
 Route::group([
         'prefix' => "api/"
     ], function (){
-        Route::post('rx/store', 'ApiController@receive');
+        Route::post('reading/store', 'ApiController@receive');
     }
 );
 
