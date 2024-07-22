80dd0b3923024c67db7499366152b03254d9da07
David Mendoza <davidmendozaofficial@gmail.com>
2024-07-22 22:06:37 +0800

    billings part 1
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/BillingController.php
+++ app/Http/Controllers/BillingController.php
@@ -3,8 +3,85 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\{Billing};
+use DB;
 
 class BillingController extends Controller
 {
-    //
+    public function __construct(){
+        $this->table = "billings";
+    }
+
+    public function index(){
+        return $this->_view('index', [
+            'title' => 'Billings'
+        ]);
+    }
+
+    public function get(Request $req){
+        $array = Billing::select($req->select);
+
+        // IF HAS SORT PARAMETER $ORDER
+        if($req->order){
+            $array = $array->orderBy($req->order[0], $req->order[1]);
+        }
+
+        // IF HAS WHERE
+        if($req->where){
+            $array = $array->where($req->where[0], $req->where[1]);
+        }
+
+        // IF HAS LIKE
+        if($req->like){
+            $array = $array->where($req->like[0], 'LIKE' ,$req->like[1]);
+        }
+
+        $array = $array->get();
+
+        // IF HAS LOAD
+        if($array->count() && $req->load){
+            foreach($req->load as $table){
+                $array->load($table);
+            }
+        }
+
+        // IF HAS GROUP
+        if($req->group){
+            $array = $array->groupBy($req->group);
+        }
+
+        echo json_encode($array);
+    }
+
+    public function store(Request $req){
+        $bill = new Billing();
+        $bill = $req->user_id;
+        $bill = $req->moxa_id;
+        $bill = $req->from;
+        $bill = $req->to;
+        $bill = $req->reading;
+        $bill = $req->rate;
+        $bill = $req->total;
+        $bill = $req->status;
+        $bill->save();
+    }
+
+    public function update(Request $req){
+        $query = DB::table($this->table);
+
+        if($req->where){
+            $query = $query->where($req->where[0], $req->where[1])->update($req->except(['id', '_token', 'where']));
+        }
+        else{
+            $query = $query->where('id', $req->id)->update($req->except(['id', '_token']));
+        }
+    }
+
+    public function delete(Request $req){
+        Billing::find($req->id)->delete();
+    }
+
+    private function _view($view, $data = array()){
+        return view('billings' . "." . $view, $data);
+    }
 }
---
+++ resources/views/billings/index.blade.php
@@ -0,0 +1,484 @@
+@extends('layouts.app')
+@section('content')
+
+<section class="content">
+    <div class="container-fluid">
+
+        <div class="row">
+            <section class="col-lg-12 connectedSortable">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            <i class="fas fa-table mr-1"></i>
+                            Billing
+                        </h3>
+
+                        @include('billings.includes.toolbar')
+                    </div>
+
+                    <div class="card-body table-responsive">
+                    	<table id="table" class="table table-hover">
+                    		<thead>
+                    			<tr>
+                    				<th>ID</th>
+                    				<th>User</th>
+                    				<th>Device</th>
+                    				<th>From</th>
+                    				<th>To</th>
+                    				<th>Reading</th>
+                    				<th>Rate</th>
+                    				<th>Total</th>
+                    				<th>Status</th>
+                    				<th>Actions</th>
+                    			</tr>
+                    		</thead>
+
+                    		<tbody>
+                    		</tbody>
+                    	</table>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+</section>
+
+@endsection
+
+@push('styles')
+	<link rel="stylesheet" href="{{ asset('css/datatables.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/select2.min.css') }}">
+	{{-- <link rel="stylesheet" href="{{ asset('css/datatables-jquery.min.css') }}"> --}}
+@endpush
+
+@push('scripts')
+	<script src="{{ asset('js/datatables.min.js') }}"></script>
+	<script src="{{ asset('js/select2.min.js') }}"></script>
+	{{-- <script src="{{ asset('js/datatables-jquery.min.js') }}"></script> --}}
+
+	<script>
+		$(document).ready(()=> {
+			var table = $('#table').DataTable({
+				ajax: {
+					url: "{{ route('datatable.moxa') }}",
+                	dataType: "json",
+                	dataSrc: "",
+					data: {
+						table: 'billings',
+						select: "*",
+						load: ['user', 'device']
+					}
+				},
+				columns: [
+					{data: 'id'},
+					{data: 'user.name'},
+					{data: 'device.name'},
+					{data: 'from'},
+					{data: 'to'},
+					{data: 'reading'},
+					{data: 'rate'},
+					{data: 'total'},
+					{data: 'status'},
+					{data: 'actions'},
+				],
+				columnDefs: [
+        			{
+        				targets: [3,4],
+        				render: value => {
+        					return moment(value).format(dateFormat);
+        				}
+        			}
+				],
+        		pageLength: 25,
+				// drawCallback: function(){
+				// 	init();
+				// }
+			});
+		});
+
+		function userDetails(id){
+			$.ajax({
+				url: "{{ route('user.get') }}",
+				data: {
+					select: '*',
+					where: ['id', id]
+				},
+				success: user => {
+					user = JSON.parse(user)[0];
+
+					console.log(user);
+
+					Swal.fire({
+						title: 'Subscriber Details',
+						html: `
+			                ${input("name", "Name", user.name, 3, 9, 'text', 'disabled')}
+			                ${input("email", "Email", user.email, 3, 9, 'text', 'disabled')}
+			                ${input("address", "Address", user.address, 3, 9, 'text', 'disabled')}
+			                ${input("contact", "Contact", user.contact, 3, 9, 'text', 'disabled')}
+						`,
+						width: '800px',
+						confirmButtonText: 'Exit',
+					});
+				}
+			})
+		}
+
+		function view(id){
+			$.ajax({
+				url: "{{ route('device.get') }}",
+				data: {
+					select: '*',
+					where: ['id', id],
+					load: ['user']
+				},
+				success: moxa => {
+					moxa = JSON.parse(moxa)[0];
+					showDetails(moxa);
+				}
+			})
+		}
+
+		function create(){
+			Swal.fire({
+				html: `
+					<div class="row iRow">
+					    <div class="col-md-3 iLabel">
+					        Subscriber
+					    </div>
+					    <div class="col-md-9 iInput">
+					        <select name="name" class="form-control">
+					        	<option value=""></option>
+					        </select>
+					    </div>
+					</div>
+					<div class="row iRow">
+					    <div class="col-md-3 iLabel">
+					        Building
+					    </div>
+					    <div class="col-md-9 iInput">
+					        <select name="category_id" class="form-control">
+					        	<option value=""></option>
+					        </select>
+					    </div>
+					</div>
+					<div class="row iRow">
+					    <div class="col-md-3 iLabel">
+					        Utility
+					    </div>
+					    <div class="col-md-9 iInput">
+					        <select name="utility" class="form-control">
+					        	<option value=""></option>
+					        </select>
+					    </div>
+					</div>
+	                ${input("serial", "Serial", null, 3, 9)}
+	                ${input("location", "Location", null, 3, 9)}
+	                ${input("floor", "Unit #", null, 3, 9)}
+				`,
+				width: '800px',
+				confirmButtonText: 'Add',
+				showCancelButton: true,
+				cancelButtonColor: errorColor,
+				cancelButtonText: 'Cancel',
+				didOpen: () => {
+					$.ajax({
+						url: "{{ route('transactionType.get') }}",
+						data: {
+							select: "*",
+							where: ['admin_id', {{ auth()->user()->id }}]
+						},
+						success: utys => {
+							utys = JSON.parse(utys);
+							utyString = "";
+
+							utys.forEach(uty => {
+								utyString += `
+									<option value="${uty.type}">${uty.type}</option>
+								`;
+							});
+
+							$("[name='utility']").append(utyString);
+							$("[name='utility']").select2({
+								placeholder: "Select Utility",
+							});
+						}
+					})
+
+					$.ajax({
+						url: "{{ route('building.getCategories') }}",
+						data: {
+							select: "*",
+							where: ['admin_id', {{ auth()->user()->id }}]
+						},
+						success: buildings => {
+							buildings = JSON.parse(buildings);
+							buildingString = "";
+
+							buildings.forEach(building => {
+								buildingString += `
+									<option value="${building.id}">${building.name}</option>
+								`;
+							});
+
+							$("[name='category_id']").append(buildingString);
+							$("[name='category_id']").select2({
+								placeholder: "Select Building"
+							});
+						}
+					})
+
+					$.ajax({
+						url: "{{ route('user.get') }}",
+						data: {
+							select: "*",
+							where: ['role', 'Subscriber']
+						},
+						success: users => {
+							users = JSON.parse(users);
+							userString = "";
+
+							users.forEach(user => {
+								userString += `
+									<option value="${user.id}">${user.name}</option>
+								`;
+							});
+
+							$("[name='name']").append(userString);
+							$("[name='name']").select2({
+								placeholder: "Select Subscriber"
+							});
+						}
+					})
+				},
+				preConfirm: () => {
+				    swal.showLoading();
+				    return new Promise(resolve => {
+				    	let bool = true;
+
+			            if($('.swal2-container input:placeholder-shown').length || $('.select2-selection__placeholder').length){
+			                Swal.showValidationMessage('Fill all fields');
+			            }
+			            else{
+			            	let bool = false;
+			            }
+
+			            bool ? setTimeout(() => {resolve()}, 500) : "";
+				    });
+				},
+			}).then(result => {
+				if(result.value){
+					swal.showLoading();
+					$.ajax({
+						url: "{{ route('device.store') }}",
+						type: "POST",
+						data: {
+							category_id: $("[name='category_id']").val(),
+							serial: $("[name='serial']").val(),
+							name: $("[name='name']").val(),
+							location: $("[name='location']").val(),
+							floor: $("[name='floor']").val(),
+							utility: $("[name='utility']").val(),
+							_token: $('meta[name="csrf-token"]').attr('content')
+						},
+						success: () => {
+							reload();
+							ss("Success");
+						}
+					})
+				}
+			});
+		}
+
+		function showDetails(moxa){
+			Swal.fire({
+				html: `
+					<div class="row iRow">
+					    <div class="col-md-3 iLabel">
+					        Subscriber
+					    </div>
+					    <div class="col-md-9 iInput">
+					        <select name="name" class="form-control">
+					        	<option value=""></option>
+					        </select>
+					    </div>
+					</div>
+					<div class="row iRow">
+					    <div class="col-md-3 iLabel">
+					        Building
+					    </div>
+					    <div class="col-md-9 iInput">
+					        <select name="category_id" class="form-control">
+					        	<option value=""></option>
+					        </select>
+					    </div>
+					</div>
+					<div class="row iRow">
+					    <div class="col-md-3 iLabel">
+					        Utility
+					    </div>
+					    <div class="col-md-9 iInput">
+					        <select name="utility" class="form-control">
+					        	<option value=""></option>
+					        </select>
+					    </div>
+					</div>
+	                ${input("id", "", moxa.user.id, 3, 9, 'hidden')}
+	                ${input("serial", "Serial #", moxa.serial, 3, 9)}
+	                ${input("location", "Location", moxa.location, 3, 9)}
+	                ${input("floor", "Unit #", moxa.floor, 3, 9)}
+				`,
+				width: '800px',
+				confirmButtonText: 'Update',
+				showCancelButton: true,
+				cancelButtonColor: errorColor,
+				cancelButtonText: 'Cancel',
+				didOpen: () => {
+					$.ajax({
+						url: "{{ route('transactionType.get') }}",
+						data: {
+							select: "*",
+							where: ['admin_id', {{ auth()->user()->id }}]
+						},
+						success: moxas => {
+							moxas = JSON.parse(moxas);
+							moxaString = "";
+
+							moxas.forEach(moxa => {
+								moxaString += `
+									<option value="${moxa.type}">${moxa.type}</option>
+								`;
+							});
+
+							$("[name='utility']").append(moxaString);
+							$("[name='utility']").select2({
+								placeholder: "Select Utility"
+							});
+
+							$("[name='utility']").val(moxa.utility).trigger('change');
+						}
+					})
+
+					$.ajax({
+						url: "{{ route('building.getCategories') }}",
+						data: {
+							select: "*",
+							where: ['admin_id', {{ auth()->user()->id }}]
+						},
+						success: buildings => {
+							buildings = JSON.parse(buildings);
+							buildingString = "";
+
+							buildings.forEach(building => {
+								buildingString += `
+									<option value="${building.id}">${building.name}</option>
+								`;
+							});
+
+							$("[name='category_id']").append(buildingString);
+							$("[name='category_id']").select2({
+								placeholder: "Select Building"
+							});
+
+							$("[name='category_id']").val(moxa.category_id).trigger('change');
+						}
+					})
+
+					$.ajax({
+						url: "{{ route('user.get') }}",
+						data: {
+							select: "*",
+							where: ['role', 'Subscriber']
+						},
+						success: users => {
+							users = JSON.parse(users);
+							userString = "";
+
+							users.forEach(user => {
+								userString += `
+									<option value="${user.id}">${user.name}</option>
+								`;
+							});
+
+							$("[name='name']").append(userString);
+							$("[name='name']").select2({
+								placeholder: "Select Subscriber"
+							});
+
+							$("[name='name']").val(moxa.name).trigger('change');
+						}
+					})
+				},
+                // showDenyButton: true,
+                // denyButtonColor: successColor,
+                // denyButtonText: 'Change Password',
+				preConfirm: () => {
+				    swal.showLoading();
+				    return new Promise(resolve => {
+				    	let bool = true;
+
+			            if($('.swal2-container input:placeholder-shown').length || $('.select2-selection__placeholder').length){
+			                Swal.showValidationMessage('Fill all fields');
+			            }
+			            else{
+			            	let bool = false;
+			            }
+
+			            bool ? setTimeout(() => {resolve()}, 500) : "";
+				    });
+				},
+			}).then(result => {
+				if(result.value){
+					swal.showLoading();
+					update({
+						url: "{{ route('device.update') }}",
+						data: {
+							id: moxa.id,
+							category_id: $("[name='category_id']").val(),
+							serial: $("[name='serial']").val(),
+							name: $("[name='name']").val(),
+							location: $("[name='location']").val(),
+							floor: $("[name='floor']").val(),
+							utility: $("[name='utility']").val(),
+						},
+						message: "Success"
+					}, () => {
+						reload();
+					});
+				}
+				else if(result.isDenied){
+					changePassword($("[name='id']").val());
+				}
+			});
+		}
+
+		function del(id){
+			sc("Confirmation", "Are you sure you want to delete?", result => {
+				if(result.value){
+					swal.showLoading();
+					update({
+						url: "{{ route('device.delete') }}",
+						data: {id: id},
+						message: "Success"
+					}, () => {
+						reload();
+					})
+				}
+			});
+		}
+
+		function updateVisibility(id, inDashboard){
+			swal.showLoading();
+			update({
+				url: "{{ route('device.update') }}",
+				data: {
+					id: id,
+					inDashboard: inDashboard ? 0 : 1
+				},
+				message: "Success"
+			},	() => {
+				reload();
+			});
+		}
+	</script>
+@endpush
