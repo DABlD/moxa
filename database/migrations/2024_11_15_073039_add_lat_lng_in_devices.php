1666d35ecd2c3b5ecb625a78e0105496f0914330
David Mendoza <davidmendozaofficial@gmail.com>
2024-11-15 07:31:05 +0800

    separate migration of lat lng
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/migrations/2024_11_15_073039_add_lat_lng_in_devices.php
+++ database/migrations/2024_11_15_073039_add_lat_lng_in_devices.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTypeInDevices extends Migration
+class AddLatLngInDevices extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class AddTypeInDevices extends Migration
     public function up()
     {
         Schema::table('devices', function (Blueprint $table) {
-            $table->string('type')->after('utility')->nullable();
+            $table->string('lat')->after('serial')->nullable();
+            $table->string('lng')->after('lat')->nullable();
         });
     }
 
@@ -26,7 +27,8 @@ class AddTypeInDevices extends Migration
     public function down()
     {
         Schema::table('devices', function (Blueprint $table) {
-            $table->dropColumn('type');
+            $table->dropColumn('lat');
+            $table->dropColumn('lng');
         });
     }
 }
